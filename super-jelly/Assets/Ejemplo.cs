5c04d36dc4c0dc61c00e2499234ace690dae6396
SlendyBones <lauti122008@hotmail.com>
2021-04-14 01:44:36 -0300

    cosaas
    cosas

---
+++ super-jelly/Assets/Ejemplo.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejemplo : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.up * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.up * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(new Vector3(0, 0, -45 * Time.deltaTime * speed));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(new Vector3(0, 0, 45 * Time.deltaTime * speed));
+        }
+    }
+}
+
