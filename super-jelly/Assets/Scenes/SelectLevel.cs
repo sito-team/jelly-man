1eca4810e7b49287792ef7d8d73306a3a066cff9
GonzaloLemos02 <gonzilemos2002@gmail.com>
2021-05-10 03:41:24 -0300

    si

---
+++ super-jelly/Assets/Scenes/SelectLevel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectLevel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void Level1()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void Level2()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+    public void mainmenu()
+    {
+        SceneManager.LoadScene("MENU");
+    }
+}
