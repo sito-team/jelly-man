f61dc5c85360305e3d6929516a0ad41841d6fdec
dalyra <82293291+dalyra@users.noreply.github.com>
2021-05-13 00:39:29 -0300

    re organizacion de todos los archivos
    reorganize los archivos en una manera mas eficiente e elimine las miles de copias de prefav que existian para renovarlas lamentablemente esto rompio algunas cosas nada dificil de solucionar (por ejemplo decirle donde esta la camara al player y asi) pero lo vi necesario habian como 20 prevavs de personaje habian como 20 texturas en la carpeta ecena y scripts era un descontrol reorganice todo y elimine lo exedente

---
+++ super-jelly/Assets/script/player_scripts/character_basic_move.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class character_basic_move : MonoBehaviour
+{
+
+    public feetcube feet;
+    public string horizontalAxis = "Horizontal";
+    public string verticalAxis = "Vertical";
+    public string jumpButtonName = "Jump";
+   
+
+
+
+    [SerializeField]
+    [Range(1.0f, 20.0f)]
+    public float movementSpeed = 2f;
+    [SerializeField]
+    [Range(1.0f, 30000.0f)]
+    public float jumpForce = 10f;
+    public ForceMode jumpForceMode = ForceMode.Impulse;
+    public Rigidbody myRigidBody;
+
+    private Vector3 inputVector;
+    [SerializeField]
+    private Transform referenceCamera;
+    public LayerMask layerMask;
+    public bool planar;
+    public int planar_fly;
+
+
+    public wind wind;
+    public bool wind_on;
+
+    public void Update()
+    {
+        wind_on = wind.wind_on_si;
+
+        if (wind_on&&planar)
+        {
+            myRigidBody.AddForce(Vector3.up * planar_fly, jumpForceMode);
+        }
+
+        inputVector = Input.GetAxis(horizontalAxis) * referenceCamera.right;
+
+        Vector3 correctedCameraForward = referenceCamera.forward;
+        correctedCameraForward.y = 0f;
+        correctedCameraForward.Normalize();
+
+        inputVector += Input.GetAxis(verticalAxis) * correctedCameraForward;
+
+       
+        jump();
+
+    }
+
+    private void FixedUpdate()
+    {
+        transform.LookAt(transform.position + inputVector);
+        myRigidBody.MovePosition(transform.position + inputVector * (movementSpeed * Time.deltaTime));
+    }
+
+    public void jump()
+    {
+
+        if (Input.GetButtonDown(jumpButtonName) && feet.grounded)
+        {
+            myRigidBody.AddForce(Vector3.up * jumpForce, jumpForceMode);
+
+        }
+    }
+ 
+}
+
