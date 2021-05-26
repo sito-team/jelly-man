b839430df38afae724a717962e580188bd587bbd
SlendyBones <lauti122008@hotmail.com>
2021-05-26 12:21:21 -0300

    Joseluili

---
+++ super-jelly/Assets/textura_asset/Mostardaño.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mostardaño : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject cosa;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.P))
+        {
+            cosa.SetActive(true);
+        }
+        else
+        {
+            cosa.SetActive(false);
+        }
+    }
+}
