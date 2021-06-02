7f5a7dd4064e5cbe32ca3b0719161137528d48bc
dalyra <82293291+dalyra@users.noreply.github.com>
2021-06-02 05:27:09 -0300

    new cinematic sistem

---
+++ super-jelly/Assets/cinemaasets/cinematicend.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using UnityEngine.Playables;
+
+public class cinematicend : MonoBehaviour
+{
+    public GameObject player;
+    public Camera cam;
+    public AudioSource goku;
+    public Camera cinematic;
+    private void Awake()
+    {
+        player.GetComponent<character_basic_move>().enabled = false;
+        goku.Play();
+        cam.enabled = false;
+        cam.GetComponent<AudioListener>().enabled = false;
+    }
+    void Update()
+    {
+        if (GetComponent<PlayableDirector>().state != PlayState.Playing || Input.GetKeyDown("space") || Input.GetKeyDown(KeyCode.Return))
+        {
+            player.GetComponent<character_basic_move>().enabled = true;
+            goku.Stop();
+            cinematic.GetComponent<AudioListener>().enabled = false;
+            cam.enabled = true;
+            cam.GetComponent<AudioListener>().enabled = true;
+        }
+
+    }
+}
