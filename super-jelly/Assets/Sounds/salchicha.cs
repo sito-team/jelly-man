022b2e5c2427bc93ab74bd4d35d051a5f514d895
dalyra <82293291+dalyra@users.noreply.github.com>
2021-06-08 17:45:38 -0300

    cosas

---
+++ super-jelly/Assets/Sounds/salchicha.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class salchicha : MonoBehaviour
+{
+    public GameObject chichicha;
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag=="box")
+        {
+            chichicha.SetActive(true);
+        }
+    }
+}
