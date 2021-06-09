530e49ea49f9fc8e5824104678c98c9c9f843d86
GonzaloLemos02 <gonzilemos2002@gmail.com>
2021-06-09 01:32:51 -0300

    you win animated y you lose animated

---
+++ super-jelly/Assets/script/menu_script/youwin.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class youwin : MonoBehaviour
+{
+    public GameObject youwin1;
+    // Start is called before the first frame update
+    public void enableyouwin()
+    {
+        youwin1.SetActive(true);
+    }
+}
