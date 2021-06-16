6e3bc1b9420f05b25c5a212d8b8a7e638cd21c82
GonzaloLemos02 <gonzilemos2002@gmail.com>
2021-06-16 09:50:41 -0300

    youwinyoulose

--- super-jelly/Assets/script/menu_script/youwin.cs
+++ super-jelly/Assets/script/menu_script/youwin.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class youwin : MonoBehaviour
 {
     public GameObject youwin1;
+    public GameObject buttom;
+    public GameObject buttom2;
     // Start is called before the first frame update
     public void enableyouwin()
     {
         youwin1.SetActive(true);
+    }
+    public void button()
+    {
+        buttom.SetActive(true);
+        buttom2.SetActive(true);
     }
 }
