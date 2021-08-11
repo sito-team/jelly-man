6ab6041b27c37cc61df395e18f28a8756b50ef18
GonzaloLemos02 <gonzilemos2002@gmail.com>
2021-08-11 01:37:01 -0300

    bala enemigo

---
+++ super-jelly/Assets/shoot.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shoot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject misilardo;
+    public Transform shootPoint;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void shootmisil()
+    {
+        Instantiate(misilardo, shootPoint.position, shootPoint.rotation);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            shootmisil();
+        }
+    }
+}
